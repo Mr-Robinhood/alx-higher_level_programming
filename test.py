b02cf070b7dd7082ce487cacfe35cd89d9a4aab9
Mr-Robinhood <donomer98@gmail.com>
2024-05-23 05:33:13 +0300

    task readme

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-global_var = 10
+class User:
+     id = 89
+     name = "no name"
+     __password = None
+     
+     def __init__(self, new_name=None):
+         self.is_new = True
+         if new_name is not None:
+             self.name = new_name
+ 
+u = User()
+u.name
 
-def func():
 
-    ans = 0
-
-    for i in range (1000):
-
-        ans+= global_var * i
-
-        return ans
-    
-func()
+print(u)
