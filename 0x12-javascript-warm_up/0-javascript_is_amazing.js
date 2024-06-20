41d80016457db09d19c066151b05e0b41396658c
Mr-Robinhood <donomer98@gmail.com>
2024-06-20 20:53:26 +0300

    task 0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myVar = "JavaScript is amazing"
-console.log (myVar)
+// Print string “Javascript is amazing”
+
+
+const myVar = "JavaScript is amazing";
+console.log (myVar);
