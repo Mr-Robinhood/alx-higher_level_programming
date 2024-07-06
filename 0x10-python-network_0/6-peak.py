8c7843b75256c4b45d142a13862578231eb76a24
Mr-Robinhood <donomer98@gmail.com>
2024-07-06 03:46:08 +0300

    task 0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,35 +2,28 @@
 
 
 def find_peak(list_of_integers):
-    """
-    Function to find a peak in a list of unsorted integers.
-    
-    Args:
-    - list_of_integers: A list of unsorted integers
-    
-    Returns:
-    - A peak element from the list
-    """
-    if not list_of_integers:
-        return None
-    
-    low, high = 0, len(list_of_integers) - 1
-    
-    while low < high:
-        mid = (low + high) // 2
-        
-        # Check if mid is a peak
-        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-           (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
-            return list_of_integers[mid]
-        
-        # If the left neighbor is greater, search left
-        elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
-            high = mid - 1
-        # Otherwise, search right
-        else:
-            low = mid + 1
-    
-    # Return the peak (in case the list has only one element)
-    return list_of_integers[low]
 
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid_idx = int(len(list_of_integers) / 2)
+
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+    else:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+        else:
+            return list_of_integers[mid_idx - 1]
+
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)
